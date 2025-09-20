51ea0f70082458287679cb6beaf4f67f06d99447
Alamin Hossain <ytakashahmed@gmail.com>
2025-09-20 15:36:51 +0600

    Updated and Server Recheck

--- course/serializers.py
+++ course/serializers.py
@@ -59,7 +59,7 @@ class CoursePurchaseSerializer(serializers.ModelSerializer):
     student_email = serializers.EmailField(source='student.email', read_only=True)
     student_name = serializers.EmailField(source='student.first_name', read_only=True)
     course_title = serializers.CharField(source='course.title', read_only=True)
-    course_price = serializers.DecimalField(source='course.price', read_only=True)
+    course_price = serializers.DecimalField(source='course.price', max_digits=10, decimal_places=2, read_only=True)
     class Meta:
         model = CoursePurchase
         fields = ['id','student_email','student_name','course','course_title','course_price','status','purchased_at']
